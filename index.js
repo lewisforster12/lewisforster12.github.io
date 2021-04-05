72b464f9e56697f1ecd832104a135716f4b9543e
lewisforster12 <44608738+lewisforster12@users.noreply.github.com>
2021-04-05 10:12:12 +0100

    First Commit Update

---
+++ index.js
@@ -0,0 +1,18 @@
+//beep.com
+console.log("User! you are in a part of the website that you are not alloud to use unless you are a dev");
+
+document.getElementById("btn1").onclick = function() {
+	function myFunction() {
+        location.replace("index.html")
+    }
+}
+document.getElementById("btn2").onclick = function() {
+    function myFunction() {
+        location.replace("about-us.html") 
+    }
+}
+document.getElementById("btn3").onclick = function() {
+	function myFunction() {
+        location.replace("https://github.com/lewisforster12")
+    }
+}
